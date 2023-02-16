9a4c88311108133f77f1a00ac135c2e883ba0be9
Alexander Chernyh <55162957+alexchernyh29@users.noreply.github.com>
2023-02-16 13:21:31 +0300

    Add files via upload

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,5 +2,29 @@ import svg4everybody from 'svg4everybody';
 import $ from 'jquery';
 
 $(() => {
-	svg4everybody();
+	const handleCloseMenu = () => {
+		$("body").removeClass("header__page_lock");
+		$(".header__burger__menu").removeClass("is-active");
+		$(".header__burger__line").removeClass("is-active");
+	};
+	
+	const handleOpenMenu = () => {
+		$("body").addClass("header__page_lock");
+		$(".header__burger__menu").addClass("is-active");
+		$(".header__burger__line").addClass("is-active");
+	};
+	
+	$(".header__menu__link").on("click", function () {
+		handleCloseMenu();
+	});
+	
+	$(".header__burger__button").on("click", function () {
+		if ($(".header__burger__line").hasClass("is-active")) {
+			handleCloseMenu();
+		} else {
+			handleOpenMenu();
+		}
+	});
 });
+
+
